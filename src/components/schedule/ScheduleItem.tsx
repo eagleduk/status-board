2b14bf07a82be84fbc76883119556083079c899b
SJ.Lee <eagleduk@gmail.com>
2023-09-07 15:53:04 +0900

    정규 / 번개 운동 형태 추가

--- src/components/schedule/ScheduleItem.tsx
+++ src/components/schedule/ScheduleItem.tsx
@@ -4,7 +4,7 @@ import { TScheduleItem } from "./type/schedule.type";
 import { IoLocationOutline } from "react-icons/io5";
 
 function ScheduleItem({
-  item: { time, hour, team, location, place, isHome },
+  item: { time, hour, team, location, place, isHome, standard },
 }: TScheduleItem) {
   const VersusText = (team: string | null, isHome: boolean) => {
     if (isHome) {
@@ -29,6 +29,7 @@ function ScheduleItem({
   return (
     <ScheduleItemStyled>
       <h4>{time && hour ? `${time}:00 ~ ${time + hour}:00` : ""}</h4>
+      {standard ? <h1> 정규 </h1> : <h1> 번개 </h1>}
       <div>
         <p>{VersusText(team, isHome)}</p>
         <p>
